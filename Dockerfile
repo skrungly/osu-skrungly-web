3f13556ae6d29e99488aa0db77867df403833d2a
kingdom5500 <kingsleymcdonald@outlook.com>
2024-02-29 01:58:01 +0000

    refactor: split up code into a package structure, lint w/ flake8.
    - routes and utils are now separate from the core flask app.
    - there is now a pre-commit hook to lint with flake8.

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,4 @@ RUN pip install -r requirements.txt
 COPY . .
 
 ENTRYPOINT ["python"]
-CMD ["app.py"]
+CMD ["web.py"]
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,14 @@
 from datetime import datetime, timedelta
-from enum import IntFlag
 import hashlib
 import os
 
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask import redirect, render_template, request, send_from_directory, url_for
 import bcrypt
-import pymysql
+
+from app import app, db
+from app.utils import Mods
 
 RECENT_SCORE_AGE = timedelta(days=1)
-
-# start with some boring database stuff
-db = pymysql.connect(
-    host="mysql",
-    user=os.environ["MYSQL_USER"],
-    password=os.environ["MYSQL_PASSWORD"],
-    db=os.environ["MYSQL_DATABASE"],
-    cursorclass=pymysql.cursors.DictCursor,
-)
 
 # TODO: maybe turn these into enums?
 WEB_MODES = {0: "osu!", 1: "osu!taiko", 2: "osu!catch", 3: "osu!mania", 4: "osu!relax"}
@@ -29,51 +21,6 @@ USER_STATS = {
 }
 
 RANKED_MODES = [2, 3]
-
-
-class Mods(IntFlag):
-    NM = 0
-    NF = 1 << 0
-    EZ = 1 << 1
-    TS = 1 << 2  # old: 'NOVIDEO'
-    HD = 1 << 3
-    HR = 1 << 4
-    SD = 1 << 5
-    DT = 1 << 6
-    RX = 1 << 7
-    HT = 1 << 8
-    NC = 1 << 9
-    FL = 1 << 10
-    # AUTOPLAY = 1 << 11
-    SO = 1 << 12
-    AP = 1 << 13
-    PF = 1 << 14
-    _4K = 1 << 15
-    _5K = 1 << 16
-    _6K = 1 << 17
-    _7K = 1 << 18
-    _8K = 1 << 19
-    FI = 1 << 20
-    RD = 1 << 21
-    # CINEMA = 1 << 22
-    TG = 1 << 23
-    _9K = 1 << 24
-    # KEYCOOP = 1 << 25
-    _1K = 1 << 26
-    _3K = 1 << 27
-    _2K = 1 << 28
-    V2 = 1 << 29
-    MR = 1 << 30
-
-    def __str__(self):
-        if not self.value:
-            return ""
-
-        return "+" + self.name.replace("|", "").replace("_", "")
-
-
-# now for the web stuff
-app = Flask(__name__)
 
 
 @app.route("/")
@@ -332,14 +279,9 @@ def user_edit_name(safe_name):
     return user_view_name(safe_name, response)
 
 
-
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(
         os.path.join(app.root_path, 'static'),
         'favicon.ico'
     )
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
