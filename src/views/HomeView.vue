76b8e1b868573000efeb51cb91ecf5a4c18a98b5
skrungly <kingsleymcdonald@outlook.com>
2025-04-30 18:31:02 +0100

    feat: add stats and a dev comment to homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,62 @@
+<script setup>
+import { onMounted, ref } from "vue"
+
+import { fetchFromAPI } from "@/api"
+
+const AVATAR_URL = import.meta.env.VITE_AVATAR_URL
+const GLOBAL_STATS = {
+  pp: "pp",
+  plays: "plays",
+  total_hits: "notes hit",
+  tscore: "total score",
+}
+
+const error = ref(null)
+const stats = ref(null)
+
+onMounted(async () => {
+  error.value = null
+
+  try {
+    var response = await fetchFromAPI("/stats")
+  } catch (e) {
+    error.value = e
+  }
+
+  stats.value = response
+})
+</script>
+
 <template>
+  <div v-if="error" class="message message--error">
+    oops :( an error occurred while fetching global stats. <span class="error-text">[{{ error }}]</span>
+  </div>
+  <section v-if="stats" class="container">
+    <div v-for="[stat, name] in Object.entries(GLOBAL_STATS)" class="stats">
+      <span class="stats__name">global {{ name }}</span>
+      <span class="stats__value">{{ stats[stat].toLocaleString() }}</span>
+    </div>
+  </section>
   <section>
-    <h2>homepage!</h2>
+    <h2>welcome!</h2>
+    <p>
+      <img class="dev-avatar" :src="`${AVATAR_URL}/3`" />
+      hey :D this is the brand new website for our private skrungly osu! server.
+      you can look around to see a number of planned features that haven't been
+      implemented yet, but i've decided to release this site for early feedback
+      and testing now that it has <i>almost</i> enough functionality to replace
+      the old one. changing your user details will be introduced alongside a
+      login system, but if you want anything changed in the meantime then just
+      let me know! ­-&nbsp;kingsley
+    </p>
   </section>
 </template>
+
+<style scoped>
+.dev-avatar {
+  margin: 0 1rem 1rem 0;
+  float: left;
+  width: 4rem;
+  border-radius: var(--border-radius);
+}
+</style>
