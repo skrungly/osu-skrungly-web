660db8b5356ede3f8b654986b6be40f29a142974
kingsley mcdonald <kingsleymcdonald@outlook.com>
2026-03-02 22:39:14 +0000

    feat: separate navigational elements from base app

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView,
+      component: () => import("../views/MainView.vue"),
+      children: [
+        {
+          path: "",
+          name: "home",
+          component: () => import("../views/HomeView.vue"),
+        },
+        {
+          path: "players",
+          name: "players",
+          component: () => import("../views/PlayerListView.vue"),
+        },
+        {
+          path: "u/:id",
+          name: "player",
+          component: () => import("../views/PlayerView.vue"),
+        },
+      ]
     },
-    {
-      path: "/players",
-      name: "players",
-      component: () => import("../views/PlayerListView.vue"),
-    },
-    {
-      path: "/u/:id",
-      name: "player",
-      component: () => import("../views/PlayerView.vue"),
-    }
   ],
 })
 
